99606dd4d74a21193c3de17f762940285d977706
emimelo <emillymelo3@gmail.com>
2023-01-01 04:21:46 -0300

    feat: Create modal

--- src/js/formDesktop.js
+++ src/js/formDesktop.js
@@ -6,26 +6,45 @@ const email = document.getElementById("emailDesk");
 const message = document.getElementById("mensagemDesk");
 
 export function validatorsDesk() {
+  let status = true;
+
   const nameValue = name.value.trim();
   const telValue = phone.value.trim();
   const emailValue = email.value.trim();
   const messageValue = message.value.trim();
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const telRegex = /^\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/;
 
-  nameValue.length < 3 ? errorValidation(name) : successValidation(name);
+  if (nameValue.length < 3) {
+    errorValidation(name);
+    status = false;
+  } else {
+    successValidation(name);
+  }
 
-  messageValue === "" ? errorValidation(message) : successValidation(message);
+  if (messageValue === "") {
+    errorValidation(message);
+    status = false;
+  } else {
+    successValidation(message);
+  }
 
-  !telRegex.test(telValue)
-    ? errorValidation(
+  if (telValue === "") {
+    errorValidation(
       phone,
-      "O Telefone deve ter o seguinte formato: '(xx) xxxxx-xxxx'"
-    )
-    : successValidation(phone);
+      "*O Telefone deve ter o seguinte formato: '(xx) xxxxx-xxxx'"
+    );
+    status = false;
+  } else {
+    successValidation(phone);
+  }
 
-  !emailRegex.test(emailValue)
-    ? errorValidation(email)
-    : successValidation(email);
+  if (!emailRegex.test(emailValue)) {
+    errorValidation(email);
+    status = false;
+  } else {
+    successValidation(email);
+  }
+
+  return status;
 }
